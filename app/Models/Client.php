601e2b2579bd18b85b92f0bb6fa9da993cb1059f
theNiba <nibaproject@gmail.com>
2024-01-29 22:36:52 +0700

    tenth commit : update database

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Client extends Authenticatable
 {
     use HasApiTokens, HasFactory, HasUuids, Notifiable;
 
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'nama',
+        'username',
         'email',
         'password',
     ];
